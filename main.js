b87da3a6c556f5af24ea8819356ccf3487ea3eac
kaneco <zegomes@gmail.com>
2020-07-04 22:20:49 +0100

    Finished UI -added colored mode slider -added reset grid button -added grid size button

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let container = document.getElementById("container");
 let items = document.getElementsByClassName("item");
+let colored = true; //set default mode to black
 
 function createGrid(dimension){ // creates dynamic css grid depending on the dimension given
     container.style.gridTemplateColumns= `repeat(${dimension}, 1fr)`;
@@ -13,6 +14,20 @@ function createGrid(dimension){ // creates dynamic css grid depending on the dim
     }
 }
 
+function paintGrid(event){ // paints the item on the grid black
+    let randomColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6); //generate random HEX color
+    if (event.target.className == "item"){
+        if (colored){ // check if colored mode is on
+            event.target.style.backgroundColor = randomColor;
+        }
+        else { // paint black if it's off
+            event.target.style.backgroundColor = "black";
+        }
+    }
+}
+
+
+
 function clearGrid(){ // removes existing grid from container (empties the container div)
     while(items.length > 0) { //because items is a HTML collection updated live, we have to use While instead of a For loop
        items[0].remove();
@@ -21,4 +36,7 @@ function clearGrid(){ // removes existing grid from container (empties the conta
     container.style.removeProperty('grid-template-rows');
 }
 
-// document.addEventListener("mouseover", )
+document.addEventListener("mouseover", function() { 
+    paintGrid(event);
+});
+
